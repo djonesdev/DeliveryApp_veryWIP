8b6e6f4c03628346a4c11c1143730d35ae2810bb
Jessica Ly <jessicaly@jessmaaoooooook.home>
2020-06-11 21:24:32 +0100

    menu list added, basket to go

--- DeliveryApp/src/pages/OrderScreen/OrderScreen.js
+++ DeliveryApp/src/pages/OrderScreen/OrderScreen.js
@@ -1,15 +1,45 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, SectionList, StyleSheet, SafeAreaView } from 'react-native'
 
-export function MenuPage({ route, navigation, resturantMenu }) {
-  alert(route.params.resturantId)
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+
+export function MenuPage({ route, navigation, resturant }) {
+  console.log(resturant[0].food, 'viewMenu')
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => navigation.push('Details')}
-        />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <SectionList
+      sections={resturant[0].food}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+      </SafeAreaView>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginHorizontal: 16
+    },
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8
+    },
+    header: {
+      fontSize: 32,
+      backgroundColor: "#fff"
+    },
+    title: {
+      fontSize: 24
+    }
+  });
+  
--- DeliveryApp/src/pages/OrderScreen/OrderScreen.page.js
+++ DeliveryApp/src/pages/OrderScreen/OrderScreen.page.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { getResturants } from '../../redux/actions';
-import { selectResturants } from '../../redux/selectors';
+import { selectResturants, selectResturant } from '../../redux/selectors';
 import { MenuPage } from './OrderScreen'
 
 function MenuPageContainer({ route, navigation, resturants, resturant }) {
-  alert(resturants)
-  console.log(resturant(route.params.resturantId, 'resturant'))
-    return <MenuPage route={route} navigation={navigation} resturantMenu={resturants} />
-  }
+  console.log(resturant(route.params.resturantId), 'resturant')
+  return <MenuPage route={route} navigation={navigation} resturant={resturant(route.params.resturantId)} />
+}
 
   const mapStateToProps = (state) =>  ({
     resturant: (resturantId) => selectResturant(state, resturantId)  
